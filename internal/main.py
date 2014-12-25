4f1888e8672cc8aa8198bef334f848d32a1c114e
MarkusHackspacher <hackspacher@gmx.de>
2014-12-25 21:37:27 +0100

    changes in main.py

--- internal/main.py
+++ internal/main.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+ransack-python
+
+Ransack - a Python based roguelike
+'''
 import pygame
 import random
 import gzip
@@ -30,7 +37,7 @@ except ImportError as err:
 # Set the height and width of the screen
 cfac = 1.0
 screenSize = [720, 700]
-screen=pygame.display.set_mode(screenSize)
+screen = pygame.display.set_mode(screenSize)
 
 if not pygame.font:
     print ('Warning, fonts disabled')
@@ -40,7 +47,7 @@ pygame.display.set_caption("Ransack")
 pygame.init()
 pygame.key.set_repeat(100, 100)
 clock = pygame.time.Clock()
-random.seed( os.urandom(1) )
+random.seed(os.urandom(1))
 
 if android:
     android.init()
@@ -52,7 +59,7 @@ C = creator.Creator()
 iH = inputHandler.inputHandler(FX)
 iFace = interface.Interface(screen, iH)
 images.load()
-D = display.Display(screen, images )
+D = display.Display(screen, images)
 
 # this is the static game world i.e. non-generated world
 # loaded once at startup and used thereafter
@@ -67,44 +74,56 @@ if pygame.font:
 y = 350
 for o in options:
     line = font.render(o, 1, colors.white, colors.black)
-    buttons.append( button.Button( ( (screen.get_width()/2)-(line.get_width()/2), y), o ) )
-    y = y + 75
+    buttons.append(button.Button(((
+        screen.get_width() / 2) - (line.get_width() / 2), y), o))
+    y += 75
 
-ifaceImg, r = load_image.load_image( os.path.join('MENU', 'interface_m.png'), None)
-logo, r = load_image.load_image( 'logo.png', None)
-        
+ifaceImg, r = load_image.load_image(os.path.join('MENU', 'interface_m.png'),
+                                    None)
+logo, r = load_image.load_image('logo.png', None)
+
+
 def endScreen(game, msg):
     spi = os.getcwd()+"/FONTS/SpinalTfanboy.ttf"
     dev = os.getcwd()+"/FONTS/devinne.ttf"
     got = os.getcwd()+"/FONTS/gothic.ttf"
-    dScreen = pygame.Surface( (screen.get_width(), screen.get_width()) )
+    dScreen = pygame.Surface((screen.get_width(), screen.get_width()))
     dScreen.fill(colors.black)
-    msgText = text.Text( msg, os.getcwd()+"/FONTS/Squealer.ttf", 18, colors.white, colors.gold )
-    dScreen.blit( text.Text( "Game Over", spi, 72, colors.red, colors.black ), (50,0) )
+    msgText = text.Text(msg, os.getcwd()+"/FONTS/Squealer.ttf", 18,
+                        colors.white, colors.gold)
+    dScreen.blit(text.Text("Game Over", spi, 72, colors.red, colors.black),
+                 (50, 0))
     font = pygame.font.Font("./FONTS/devinne.ttf", 18)
     if game.myHero.level < 4:
-        dScreen.blit( text.Text( "Nice Try, loser!", dev, 18, colors.white, colors.black ), (50,200) )
+        dScreen.blit(text.Text("Nice Try, loser!", dev, 18,
+                     colors.white, colors.black), (50, 200))
     elif game.myHero.level >= 4 and game.myHero.level < 10:
-        dScreen.blit( text.Text( "Not bad... for a beginner!", dev, 18, colors.white, colors.black), (50,350) )
-    dScreen.blit( text.Text("Level reached: "+str(game.myHero.level), got, 18, colors.white, colors.black), (50,500) )
-    dScreen.blit( text.Text(str(game.Ticker.getDays())+"days, "+
-                            str(game.Ticker.getHours()%24)+":"+
-                            str(game.Ticker.getMins()%60)+"."+
-                            str(game.Ticker.getSecs()), got, 14, colors.white, colors.black), (50,250) )
+        dScreen.blit(text.Text("Not bad... for a beginner!", dev, 18,
+                     colors.white, colors.black), (50, 350))
+    dScreen.blit(text.Text("Level reached: {}".format(game.myHero.level),
+                 got, 18, colors.white, colors.black), (50, 500))
+    dScreen.blit(text.Text("{} days, {}:{}.{}".format(
+                           game.Ticker.getDays(),
+                           game.Ticker.getHours() % 24,
+                           game.Ticker.getMins() % 60,
+                           game.Ticker.getSecs()),
+                           got, 14, colors.white,
+                           colors.black), (50, 250))
     '''
-    screen.blit(pygame.transform.scale(dScreen, (int(ceil(300 * 2.4)), 
-                                                 int(ceil(300 * 2.4)) ) ), 
+    screen.blit(pygame.transform.scale(dScreen, (int(ceil(300 * 2.4)),
+                                                 int(ceil(300 * 2.4)))),
                                                  (0, 0) )
                                                  '''
-    screen.blit(dScreen, (0, 0) )
+    screen.blit(dScreen, (0, 0))
     pygame.display.flip()
     while (pygame.event.wait().type != pygame.MOUSEBUTTONDOWN):
         pass
 
+
 def launchNewGame(titleScreen):
     print('launchNewGame')
     newGame = game.game(images, screen, clock, iFace, FX, iH, titleScreen, SFX,
-        myWorldBall, loadHero=C.mainLoop(screen))
+                        myWorldBall, loadHero=C.mainLoop(screen))
     FX.fadeOut(0)
     iFace.state = 'game'
     if newGame.mainLoop():
@@ -112,29 +131,34 @@ def launchNewGame(titleScreen):
     else:
         endScreen(newGame, "Game Over.")
     FX.fadeOut(0)
-    
+
+
 def loadWorld():
     try:
         if android:
             pass
-        loadedWorld = gzip.GzipFile(os.getcwd()+'/MAP/WORLDS/MainWorld', 'rb', 1)
+        loadedWorld = gzip.GzipFile(os.getcwd() + '/MAP/WORLDS/MainWorld',
+                                    'rb', 1)
         myWorldBall = cPickle.load(loadedWorld)
         loadedWorld.close()
-        #self.installWorldBall(ball, context)
+        # self.installWorldBall(ball, context)
     except IOError as err:
         print('Cannot load world: {}'.format(err))
         return
-        
+
+
 def loadSavedGame(titleScreen):
     if android:
-         android.hide_keyboard()
+        android.hide_keyboard()
     try:
         FX.displayLoadingMessage(titleScreen, 'Loading game file...')
         savFile = gzip.GzipFile('ransack0.sav', 'rb', 1)
         FX.displayLoadingMessage(titleScreen, 'Loading saved game...')
         ball = cPickle.load(savFile)
         savFile.close()
-        Game = game.game(images, screen, clock, iFace, FX, iH, titleScreen, SFX, myWorldBall, ball[0], ball[1], ball[2], ball[3])
+        Game = game.game(images, screen, clock, iFace,
+                         FX, iH, titleScreen, SFX, myWorldBall,
+                         ball[0], ball[1], ball[2], ball[3])
         FX.fadeOut(0)
         iFace.state = 'game'
         if Game.mainLoop():
@@ -142,9 +166,9 @@ def loadSavedGame(titleScreen):
         else:
             endScreen(Game, "Game Over.")
         FX.fadeOut(0)
-    except IOError as e:
-        print ('loadSavedGame error: {}'.format(e))
-    
+    except IOError as err:
+        print ('loadSavedGame error: {}'.format(err))
+
 
 def mouseHandler(m):
     (mx, my) = pygame.mouse.get_pos()
@@ -154,6 +178,7 @@ def mouseHandler(m):
         loadSavedGame()
     elif (230 <= mx < 300) and (485 <= my < 540):
         os.sys.exit()
+
 
 def updateDisplay():
     titleScreen = pygame.Surface((screenSize[0], screenSize[1]))
@@ -167,22 +192,37 @@ def updateDisplay():
     if pygame.font:
         font = pygame.font.Font("./FONTS/SpinalTfanboy.ttf", 48)
         for b in buttons:
-            screen.blit(b.img, ( b.locX, b.locY ) )
-            #line = font.render(options[i], 1, colors.white, colors.black)
-            #menuBox.blit(line, (30, (i * line.get_height())))
+            screen.blit(b.img, (b.locX, b.locY))
+            # line = font.render(options[i], 1, colors.white, colors.black)
+            # menuBox.blit(line, (30, (i * line.get_height())))
 
-
-    #menuBox = pygame.Surface( (450,450) )
-    #menuBox.fill( colors.black )
-    #menuBox.set_colorkey(colors.black)
+    # menuBox = pygame.Surface( (450,450) )
+    # menuBox.fill( colors.black )
+    # menuBox.set_colorkey(colors.black)
 
 
 def showCredits():
     creditsMenu = menu.menu(screen, iH, D, iFace, FX, SFX)
-    creditsMenu.displayStory("Ransack - An RPG Roguelike. All game code, story (if you can call it that!) and artwork, with the exclusion of fonts, created by D. Allen. dsallen7@gmail.com Powered by Python - www.python.org Game engine built with Pygame - pygame.org and ported to Android using PGS4a - pygame.renpy.org/")
-    creditsMenu.displayStory("Fonts used in game: Steelfish, Sqeualer by Ray Larabie, Typodermic Fonts - http://www.dafont.com/ typodermic.d1705 Gothic and Chancery by URW Software. urwpp.de/")
-    creditsMenu.displayStory("I'm a big fan of all the classic roguelikes, and cult favorites like Castle of the Winds and Moraff's World, as well as Japanese-style RPGs, from Final Fantasy to Suikoden to Pokemon. So, you could call this a melting pot of all my gaming influences.")
-    creditsMenu.displayStory("This is a work in progress. I thank you for playing and if you notice any bugs or errors, have any ideas for improvments or enhancenments please drop me a line!")
+    creditsMenu.displayStory(
+        "Ransack - An RPG Roguelike. All game code, story (if you can call it"
+        " that!) and artwork, with the exclusion of fonts, created by"
+        " D. Allen. dsallen7@gmail.com Powered by Python - www.python.org"
+        " Game engine built with Pygame - pygame.org and ported to Android"
+        " using PGS4a - pygame.renpy.org/")
+    creditsMenu.displayStory(
+        "Fonts used in game: Steelfish, Sqeualer by Ray"" Larabie,"
+        " Typodermic Fonts - http://www.dafont.com/ typodermic.d1705 Gothic"
+        " and Chancery by URW Software. urwpp.de/")
+    creditsMenu.displayStory(
+        "I'm a big fan of all the classic roguelikes, and"" cult favorites"
+        " like Castle of the Winds and Moraff's World, as well as"
+        " Japanese-style RPGs, from Final Fantasy to Suikoden to Pokemon."
+        " So, you could call this a melting pot of all my gaming influences.")
+    creditsMenu.displayStory(
+        "This is a work in progress. I thank you for"" playing and if you"
+        " notice any bugs or errors, have any ideas for improvments or"
+        " enhancenments please drop me a line!")
+
 
 def main():
     while True:
@@ -194,21 +234,24 @@ def main():
                     if b.hit(mX, mY):
                         selection = b.msg
                 if selection == 'Begin New Game':
-                    print ('Begin New Game {}x{}'.format(screenSize[0],screenSize[1]))
-                    launchNewGame(pygame.Surface((screenSize[0],screenSize[1])))
+                    print('Begin New Game {}x{}'.format(screenSize[0],
+                          screenSize[1]))
+                    launchNewGame(pygame.Surface((screenSize[0],
+                                  screenSize[1])))
                 elif selection == 'Load Saved Game':
-                    loadSavedGame(pygame.Surface((screenSize[0],screenSize[1])))
+                    loadSavedGame(pygame.Surface((screenSize[0],
+                                  screenSize[1])))
                 elif selection == 'Credits':
                     showCredits()
                 elif selection == 'Exit':
                     FX.fadeOut(0)
                     os.sys.exit()
-                elif selection == None:
+                elif selection is None:
                     pass
-        #iFace.update()
         font = pygame.font.Font(os.getcwd()+"/FONTS/courier.ttf", 28)
         if android:
-            screen.blit( font.render( str( android.get_dpi() ), 1, colors.white, colors.black ), (0,0) )
+            screen.blit(font.render(str(android.get_dpi()), 1, colors.white,
+                        colors.black), (0, 0))
         updateDisplay()
         pygame.display.flip()
 
